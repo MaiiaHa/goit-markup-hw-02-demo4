cc00cb2bac8d25ac7713e559388c2ed9c6df5295
MaiiaHa <106480484+MaiiaHa@users.noreply.github.com>
2022-09-27 15:09:33 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const firstArray = ["Mango", "Poly"];
+const secondArray = ["Ajax", "Chelsea"];
+const maxLength = "3";
+const array = firstArray.concat(secondArray);
+const makeArray = array.slice(0, maxLength - 1);
+console.log(array);
